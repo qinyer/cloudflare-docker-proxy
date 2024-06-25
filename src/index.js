cddeb2850647e5e8acbc444dafc43dc9482d2a86
qinyer <39786812+qinyer@users.noreply.github.com>
2024-06-25 16:11:45 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.tabqin.com": dockerHub,
+  "quay.tabqin.com": "https://quay.io",
+  "gcr.tabqin.com": "https://gcr.io",
+  "k8s-gcr.tabqin.com": "https://k8s.gcr.io",
+  "k8s.tabqin.com": "https://registry.k8s.io",
+  "ghcr.tabqin.com": "https://ghcr.io",
+  "cloudsmith.tabqin.com": "https://docker.cloudsmith.io",
+  "ecr.tabqin.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.tabqin.com": dockerHub,
 };
 
 function routeByHosts(host) {
